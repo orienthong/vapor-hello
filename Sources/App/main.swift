567e49112db5b809811c962850bf157833ddb22b
scauos <645670644@qq.com>
2016-10-19 21:41:11 +0800

    Git itit

---
+++ Sources/App/main.swift
@@ -0,0 +1,32 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { _ in
+    //return "Hello Vapor"
+    return try JSON(node: [
+        "message": "Hello, Vapor"
+        ])
+}
+drop.get("Hello", "there") { request in // Hello/there
+    return try JSON (node: [
+        "message": "Hello Vappr ahh"
+        ])
+}
+
+drop.get("beers", Int.self) { request, beers in
+    return try JSON(node: [
+        "message": "take one down, pass it around, \(beers - 1) bottles of beer on the wall..."
+        ])
+}
+
+drop.post("post") { request in
+    guard let name = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    return try JSON(node: [
+        "name": "Hello \(name)!"
+        ])
+}
+
+drop.run()
